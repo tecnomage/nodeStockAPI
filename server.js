a819382ebf3753cb7092060334a25db2cf6c1fc6
tecnomage <tecnocratay@gmail.com>
2019-12-19 13:50:14 -0300

    still not workint

--- server.js
+++ server.js
@@ -1,105 +1,107 @@
 //const http = require('http');
-const fetch = require('node-fetch')
-const request = require('request')
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors');
+const fetch = require("node-fetch");
+const request = require("request");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const port = 8282
+const port = 8282;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
 //app.use(express.bodyParser());
 
 // var corsOptions = {
-  //   origin: 'http://example.com',
-  //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  // }
-  
- //app.use(cors);
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
- app.get("/",(req,res,next)=>{
-  res.json(["teste","de", "acesso"]);
-})
+//app.use(cors);
 
+app.get("/", (req, res, next) => {
+  res.json(["teste", "de", "acesso"]);
+});
 
-app.get("/acao", cors() ,async (req, res, next)=>{
-  console.log('entrou')
-  request('https://jsonplaceholder.typicode.com/todos',(err, body)=>{
+app.get("/acao", cors(), async (req, res, next) => {
+  console.log("entrou");
+  request("https://jsonplaceholder.typicode.com/todos", (err, body) => {
     //console.log(body.body)
-     return res.json(body.body)
+    return res.json(body.body);
     //return res.send({todo:[12,3,4,5,6,7,8,9]})
-    
-  })
-  
-})
-
+  });
+});
 
 //TODO o front deve passar uma listagem com os codigos das acoes que irei listar
-app.get('/stock/:acao',cors(), async (req, res, next)=>{
-  console.log('entrou2')
-  var acao = req.params.acao
-  let parsed = {}
-  console.log(acao)
-  request(`http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,(err, body)=>{
-   var dados_da_acao = body.body
-   //res.json(body.body)
-   parsed = JSON.parse(dados_da_acao)
-   return res.json(body.body)
-   
-  })
-})
+app.get("/stock/:acao", cors(), async (req, res, next) => {
+  console.log("entrou2");
+  var acao = req.params.acao;
+  let parsed = {};
+  console.log(acao);
+  request(
+    `http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,
+    (err, body) => {
+      var dados_da_acao = body.body;
+      //res.json(body.body)
+      parsed = JSON.parse(dados_da_acao);
+      return res.json(body.body);
+    }
+  );
+});
 
+app.get("/stock/:acao", cors(), async (req, res, next) => {
+  console.log("entrou2");
+  var acao = req.params.acao;
+  let parsed = {};
+  console.log(acao);
+  request(
+    `http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,
+    (err, body) => {
+      var dados_da_acao = body.body;
+      //res.json(body.body)
+      parsed = JSON.parse(dados_da_acao);
+      return res.json(body.body);
+    }
+  );
+});
 
+app.post("/stocks/", cors(), async (req, res) => {
+  console.log("entrou 3");
+  // console.log(req.body)
+  var acoes = req.body.acoes;
+  var novos = {};
+  //console.log(acoes);
+  var saida = [];
+  // novos =
+   await acoes.map(acao => {
+    request(
+      `http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,
+      (err, body) => {
+        var dados_da_acao = body.body;
+        var parsed = JSON.parse(dados_da_acao);
+        //console.log(parsed)
+        //novos.push(parsed)
+        //console.log(dados_da_acao)
+        //novos.push(parsed);
+        saida.push({acao: parsed});
+         novos = {...parsed};
+        //console.log(saida)
+      }
+    );
+    //console.log(saida)
+    console.log(novos)
+    return { acao: saida };
+  });
+  console.log(saida);
+  console.log(novos);
 
-app.get('/stock/:acao',cors(), async (req, res, next)=>{
-  console.log('entrou2')
-  var acao = req.params.acao
-  let parsed = {}
-  console.log(acao)
-  request(`http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,(err, body)=>{
-   var dados_da_acao = body.body
-   //res.json(body.body)
-   parsed = JSON.parse(dados_da_acao)
-   return res.json(body.body)
-   
-  })
-})
+  return res.send(saida);
+});
 
-
-app.post('/stocks/',cors(), (req, res)=>{
-  console.log('entrou 3')
-    // console.log(req.body)
-    var acoes = req.body.acoes.map( acao => {
-      request(`http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,(err, body)=>{
-       var dados_da_acao = body.body
-       //res.json(body.body)
-       parsed = JSON.parse(dados_da_acao)
-       console.log(dados_da_acao)
-       return parsed
-       })
-
-    })
-    console.log(acoes)
-    // request(`http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,(err, body)=>{
-      //  var dados_da_acao = body.body
-      //  //res.json(body.body)
-      //  parsed = JSON.parse(dados_da_acao)
-      //  return res.json(body.body)
-      
-      return res.send(acoes);
-  //
-})
-
-
-
-app.listen(port, ()=> {
-  console.log(`hello Server, estou ouvindo na porta ${port}`)
-})
-
- 
+app.listen(port, () => {
+  console.log(`hello Server, estou ouvindo na porta ${port}`);
+});
 
 // req.query: directly access the parsed query string parameters
 // req.params: directly access the parsed route parameters from the path
