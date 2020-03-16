972b677b8df05dc7df7e582b2063ebd4836a798c
tecnomage <tecnocratay@gmail.com>
2020-03-16 13:10:59 -0300

    enable cors

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(bodyParser.raw());
 
 //app.use(express.bodyParser());
 
-// var corsOptions = {
-//   origin: 'http://example.com',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+var corsOptions = {
+  origin: 'http://localhost:3000/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 app.use(cors());
 
@@ -54,6 +54,7 @@ app.get("/stock/:acao", cors(), async (req, res, next) => {
 app.get("/stock/:acao", cors(), async (req, res, next) => {
   console.log("entrou2");
   var acao = req.params.acao;
+  console.log(acao)
   let parsed = {};
   console.log(acao);
   request(
@@ -67,16 +68,24 @@ app.get("/stock/:acao", cors(), async (req, res, next) => {
   );
 });
 
-app.post("/stocks/", cors(), async (req, res) => {
+app.post("/stocks/", cors(corsOptions), async (req, res) => {
   var acoes = req.body.acoes;
   var novos = {};
-
+  console.log(acoes)
   var saida = [];
   var lista = [];
+  
+  //TODO1 verificar se acoes tem dados
+  // if (
+  //   rendafixa &&
+  //   Object.entries(rendafixa) &&
+  //   operacao &&
+  //   Object.entries(operacao)
+  // )
 
   busca_acoes(acoes, valor => {
     lista = [...lista, valor];
-    console.log(valor);
+    //console.log(valor);
     if (lista.length === acoes.length) {
       res.send(lista);
     }
@@ -87,6 +96,9 @@ app.post("/stocks/", cors(), async (req, res) => {
 
   async function busca_acoes(acoes, callback) {
     var parsed;
+    //TODO O ERRO ESTÁ AQUI map of undefined
+    if(Object.entries(acoes) && acoes){
+      console.log(acoes)
     const saida = acoes.map(acao => {
       request(
         `http://webservices.infoinvest.com.br/cotacoes/cotacoes_handler.asp?&quotes=&quotes=sp.${acao}`,
@@ -97,8 +109,9 @@ app.post("/stocks/", cors(), async (req, res) => {
         }
       );
 
-      //return lista;
+      return lista;
     });
+  }
   }
 });
 
